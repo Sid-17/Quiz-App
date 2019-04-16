78cd75fefcbfe5170a82207e588e64d45be7ad49
siddhant.shah <siddhant.shah1996@gmail.com>
2019-04-16 23:16:40 +0530

    first commit

---
+++ Quiz-App/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { LoginComponent } from './login/login.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Quiz-App/src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit} from '@angular/core';
+import { NgModule } from '@angular/core';
+import { SinglePropOffsetValuesIndex } from '@angular/core/src/render3/interfaces/styling';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css', './signin.css', './bootstrap.css']
+})
+export class LoginComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
