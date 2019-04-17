f077d97a2e823fc3c783ccd5c3777f68b4096831
siddhant.shah <siddhant.shah1996@gmail.com>
2019-04-17 22:26:00 +0530

    new component added

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit} from '@angular/core';
+import { NgModule } from '@angular/core';
+import { SinglePropOffsetValuesIndex } from '@angular/core/src/render3/interfaces/styling';
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
